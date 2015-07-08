5418ae52bb37108c54b7230ec5652e6c4d101105
arundh93 <arun.dh93@gmail.com>
2015-07-08 23:30:43 +0530

    prime factor example added

--- IntroductoryProgrammingExercises/src/FizzBizz.java
+++ IntroductoryProgrammingExercises/src/FizzBizz.java
@@ -2,4 +2,25 @@
  * Created by arun on 1/7/15.
  */
 public class FizzBizz {
+    public static void main(String[] args) {
+        int i,n;
+        n=100;
+        FizzBuzz(n);
+    }
+
+    private static void FizzBuzz(int n) {
+        int i;
+        for(i=1;i<=n;i++){
+            if(i%3==0 && i%5==0)
+                System.out.println("FizzBuzz");
+            else if (i%3==0){
+                System.out.println("Fizz");
+            }
+            else if (i%5==0){
+                System.out.println("Buzz");
+            }
+            else
+                System.out.println(i);
+        }
+    }
 }
--- IntroductoryProgrammingExercises/src/PrimeFactors.java
+++ IntroductoryProgrammingExercises/src/PrimeFactors.java
@@ -1,5 +1,41 @@
+import java.util.ArrayList;
+
 /**
  * Created by arun on 1/7/15.
  */
 public class PrimeFactors {
+    public static void main(String[] args) {
+        int i, n, k, j;
+        ArrayList primeNoList=new ArrayList();
+        ArrayList primeFactorList=new ArrayList();
+
+        n = 30;
+        primeNoList=primeNumberGenerator(n);
+        for(i=0;i<primeNoList.size();i++){
+            if(n%(int)primeNoList.get(i)==0)
+                primeFactorList.add(primeNoList.get(i));
+        }
+        System.out.println(primeFactorList);
+    }
+
+    private static ArrayList primeNumberGenerator(int number) {
+        ArrayList prime=new ArrayList();
+        boolean test;
+        for (int i = 3; i <= number; i++) {
+            test=true;
+            for (int j = 2; j < i; j++) {
+                if(i%j==0){
+
+                    test=false;
+                    break;
+                }
+            }
+            if(i==3)
+                prime.add(i-1);
+            if(test==true){
+                prime.add(i);
+            }
+        }
+        return prime;
+    }
 }
